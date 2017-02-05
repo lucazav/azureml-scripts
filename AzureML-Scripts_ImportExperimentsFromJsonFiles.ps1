d179f6fa1f1dfa26d09d877f4ed5ecb05d1e441a
Luca <lucazav@users.noreply.github.com>
2017-02-05 23:42:55 +0100

    Added comments to the import json script

--- AzureML-Scripts_ImportExperimentsFromJsonFiles.ps1
+++ AzureML-Scripts_ImportExperimentsFromJsonFiles.ps1
@@ -5,10 +5,17 @@ Unblock-File C:\Install\AzureMLPS\AzureMLPS.dll
 Import-Module C:\Install\AzureMLPS\AzureMLPS.dll
 
 
+# Define the folder containing all the experiment json files
 $sourceFolder = 'c:\Temp\';
+
+# Get the destination workspace
 $destinationWorkspace = Get-AmlWorkspace -ConfigFile 'C:\Install\AzureMLPS\config_destination.json'
+
+# Define a boolean variable. If true, the experiments will be imported using the file name.
+# If false, they will be imported using the experiment original name
 $useFileNames = $true;
 
+# Pop out the gui to select one or more experiment json files. Then import them in the destination workspace
 if (!$useFileNames)
 {
     dir "$sourceFolder\*.json" | out-gridview -Title "JSON Files of the Source Experiments" -passthru `
